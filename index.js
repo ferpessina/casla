8cd7c407e5b54b4e1885690363a3963c6366435e
ilan <ilanrosenfeld7@gmail.com>
2017-01-23 11:40:03 -0300

    adding structure

---
+++ index.js
@@ -0,0 +1,40 @@
+var express         = require("express"),
+    app             = express(),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose'),
+    morgan       	  = require('morgan'),
+    passport 		    = require('passport'),
+	  flash    		    = require('connect-flash'),
+	  cookieParser 	  = require('cookie-parser'),
+	  session      	  = require('express-session'),
+    bodyParser      = require('body-parser'),
+    port            = process.env.PORT || 8080;
+
+// Connection to DB
+mongoose.connect('mongodb://localhost/casla', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'lancha-dante' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./config/routes.js')(express,app, passport); // load our routes and pass in our app and fully configured passport
+
+// Start server
+app.listen(3000, function() {
+  console.log("Node server running on "+port);
+});
