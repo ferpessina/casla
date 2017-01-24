78a194e762858220449ff7446cd59ea4d259e342
ilan rosenfeld <ilanrosenfeld7@gmail.com>
2017-01-24 12:36:55 -0300

    removing all

--- models/includingModels.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app, mongoose) {
-	var jugador   = require('./jugador')(app, mongoose);
-	var equipo   = require('./equipo')(app, mongoose);
-	var torneo   = require('./torneo')(app, mongoose);
-}
