28f66ddb9fd0a59118bc0de28a30152f6bb42275
ilan <Ilan.X.Rosenfeld.-ND@disney.com>
2017-01-24 09:49:41 -0300

    adding old project

---
+++ old/menu.php
@@ -0,0 +1,129 @@
+          <div class="container">
+            <div class="row">
+              <h1 class="logo">
+                <a href="index.php">
+                  <img src="images/logo.png" alt="image">
+                </a>
+              </h1>
+              <div class="menubar">
+                 <nav class="navbar">
+                      <div class="nav-wrapper">
+                        <div class="navbar-header">
+                          <button type="button" class="navbar-toggle" >
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                          </button>      
+                        </div>
+                        <div class="nav-menu" >
+                          <!-- <a href="#" class="close">
+                            <i class="fa fa-times"></i>
+                          </a> -->
+                          <ul class="nav navbar-nav menu-bar">
+                            <li>
+                              <a href="index.php">
+                              Inicio
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              </a>
+                            </li>
+                            <li>
+                              <a>Fixture 
+							  <span></span> 
+							  <span></span> 
+							  <span></span> 
+							  <span></span>
+							  </a>
+                            <ul class="sub-menu">
+							<li><a href="#">Zona 1: SERGIO VILLAR</a></li>
+							<li><a href="#">Zona 2: HECTOR SCOTTA</a></li>
+							</ul>
+                            <li>
+                              <a href="goleadores.php">
+                              Goleadores
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              </a>
+                            </li>
+                             <li>
+                              <a href="sanciones.php">
+                              Sanciones
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              </a>
+                            </li>
+                              <li> 
+							   <a href="fairplay.php">
+                               Fair Play
+                               <span></span>
+                               <span></span>
+                               <span></span>
+                               <span></span>
+                               </a>
+                             </li>
+							<li>
+                              <a href="#">
+                              Notas
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              </a>
+                            </li>
+							 <li>
+                             <a href="https://www.facebook.com/Copa-Viejo-Gasometro-238053553016980/timeline" target="_blank" >
+                              Galeria
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              </a></li>
+                              <!--<ul class="sub-menu">
+                                <li><a href="gallerypage01.html">masonry</a></li>
+                                <li><a href="gallery02.html">gallery column two</a></li>
+                                <li><a href="gallery03.html">gallery column 03</a></li>
+                              </li>
+							
+							 </ul> -->
+                                 <li>
+                              <a href="#">
+                              info del Torneo
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                              </a>
+                            </li>
+                            <!-- <li>
+                               <a>
+                               error
+                               <span></span>
+                               <span></span>
+                               <span></span>
+                               <span></span>
+                               </a>
+                               <ul class="sub-menu">
+                                 <li><a href="400.html">400 page</a></li>
+                                 <li><a href="401.html">401 page</a></li>
+                                 <li><a href="403.html">403 page</a></li>
+                                 <li><a href="404.html">404 page</a></li>
+                                 <li><a href="500.html">500 page</a></li>
+                                 <li><a href="503.html">503 page</a></li>
+                               </ul>
+                             </li>-->
+                          </ul>
+                        </div>
+                      </div>  
+                    </nav>
+              </div>
+              <div class="social">
+                <a href="https://www.facebook.com/Copa-Viejo-Gasometro-238053553016980/timeline" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a>
+                <a href="https://twitter.com/RecreativoCASLA" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a>
+              </div>
+            </div>  
+          </div>
